26374a905cbb91711e1405f27dd9bc50e7859190
Gek Siong Low <geksiong@yahoo.com>
2021-09-19 13:41:05 +0800

    chore: Update icon references in index.html and VitePWA

--- vite.config.ts
+++ vite.config.ts
@@ -100,22 +100,24 @@ export default defineConfig({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.svg', 'robots.txt', 'safari-pinned-tab.svg'],
       manifest: {
-        name: 'Vitesse',
-        short_name: 'Vitesse',
-        theme_color: '#ffffff',
+        name: 'Vitesse More',
+        short_name: 'Vitesse More',
+        theme_color: '#ffc40d',
+        background_color: '#ffc40d',
+        display: 'standalone',
         icons: [
           {
-            src: '/pwa-192x192.png',
+            src: '/android-chrome-192x192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/pwa-512x512.png',
+            src: '/android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png',
           },
           {
-            src: '/pwa-512x512.png',
+            src: '/android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable',
